13e2579d0a1ed540f2dbb8dfe3520db6f0d1d373
gavrilasa <gavrilasamanaa@gmail.com>
2024-12-24 14:48:03 +0700

    Add Category and Delete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const path = require('path')
 const app = express()
@@ -15,14 +16,21 @@ mongoose.connect('mongodb://127.0.0.1/shop_db')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.send('Project Set Up Succeed')
 })
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', { products })
+    const { category } = req.query
+    if (category) {
+        const products = await Product.find({ category })
+        res.render('products/index', { products, category })
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'All' })
+    }
 })
 
 app.get('/products/create', (req, res) => {
@@ -45,6 +53,18 @@ app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params
     const product = await Product.findById(id)
     res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true })
+    res.redirect(`/products/${product._id}`)
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params
+    await Product.findByIdAndDelete(id)
+    res.redirect('/products')
 })
 
 app.listen(3000, () => {

600305c1b4b42f1a343a00338d7fd311cff3c30d
gavrilasa <gavrilasamanaa@gmail.com>
2024-12-24 12:43:10 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const app = express()
+
+mongoose.connect('mongodb://127.0.0.1/shop_db')
+    .then((result) => {
+        console.log('Connected to MongoDB')
+    }).catch((err) => {
+        console.log(err)
+    })
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.send('Project Set Up Succeed')
+})
+
+app.listen(3000, () => {
+    console.log('ShopApp listening on http://127.0.0.1:3000')
+})
